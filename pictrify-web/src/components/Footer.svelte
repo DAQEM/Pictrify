3d5a64d58c179798e217c6b432dfbeb466baaefa
DAQEM <daqemyt@gmail.com>
2023-05-19 02:04:32 +0200

    Added Git ignore to auth project

---
+++ pictrify-web/src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
+</script>
+
+<Footer style="border-radius: 0">
+	<FooterCopyright href="/" by="Flowbite™" year={2022} />
+	<FooterLinkGroup
+		ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+	>
+		<FooterLink href="/">About</FooterLink>
+		<FooterLink href="/">Privacy Policy</FooterLink>
+		<FooterLink href="/">Licensing</FooterLink>
+		<FooterLink href="/">Contact</FooterLink>
+	</FooterLinkGroup>
+</Footer>
